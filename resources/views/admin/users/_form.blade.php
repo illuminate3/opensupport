e16372fef4416ec9c519348f0315e320196e6c35
Steffen Maegaard <taocoder@gmail.com>
2015-03-03 23:09:08 +0100

    password field hidden with user admin fix #27

--- resources/views/admin/users/_form.blade.php
+++ resources/views/admin/users/_form.blade.php
@@ -8,11 +8,11 @@
 </div>
 <div class="form-group">
     {!! Form::label('password', 'Password:') !!}
-    {!! Form::text('password', null, ['class' => 'form-control']) !!}
+    {!! Form::password('password', null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
     {!! Form::label('password2', 'Password:') !!}
-    {!! Form::text('password2', null, ['class' => 'form-control']) !!}
+    {!! Form::password('password2', null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
     {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary form-control']) !!}
