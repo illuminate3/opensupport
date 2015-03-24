0b9dca2add4bfed56bcd7b93e972488970c157e8
Steffen Maegaard <taocoder@gmail.com>
2015-03-24 19:59:34 +0100

    Fix #36, not super pretty but workable. Starte ticket Timeline.

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -2,94 +2,92 @@
 
 @section('content')
 
-<h1>Ticket list</h1>
 
-<a href="{{ action('TicketController@create') }}" class="btn btn-primary" role="button">New</a>
-<a href="#" class="btn btn-primary" role="button">Merge</a>
-<a href="#" class="btn btn-primary" role="button">Delete</a>
-<a href="#" class="btn btn-primary" role="button">Close</a>
-<a href="#" class="btn btn-primary" role="button">Assigned to</a>
+<div class="box box-solid">
+    <div class="box-header with-border">
 
-<hr/>
-
-@if ( count($tickets) )
-
-<div class="box">
-    <div class="box-header">
         <h3 class="box-title">Ticket list</h3>
-    </div><!-- /.box-header -->
+    </div>
     <div class="box-body">
-        <table id="tickets" class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th>Select</th>
-                    <th>Title</th>
-                    <th>Create</th>
-                    <th>Owner</th>
-                    <th>Client</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ( $tickets as $ticket )
-                <tr>
-                    <!-- Select -->
-                    <td>
-                        <input type="checkbox" name="option1" value="Selected"> 
-                    </td>
-                    <!-- Title -->
-                    <td>
-                        <div class="text-left">
-                            <a href="{{ action('TicketController@show', $ticket->id ) }}"> {{ $ticket->title }}</a> 
-                        </div>
-                    </td>
-                    <!-- Create -->
-                    <td>
-                        {{ $ticket->created_at->diffForHumans() }}
-                    </td>
-                    <!-- Owner -->
-                    <td>
-                        @if ($ticket->assignedTo == null )
-                        <h5>unassigned</h5>
-                        @else
-                        <h5>{{ $ticket->assignedTo->name }}</h5>
-                        @endif
-                    </td>
-                    <!-- Client -->
-                    <td>
-                        @if ( $ticket->isFrom == null )
-                        <h5>unassigned</h5>
-                        @else
-                        <h5>{{ $ticket->isFrom->name }}</h5>
-                        @endif
-                    </td>
-                    <!-- Status -->
-                    <td>
-                        <h5>{{ $ticket->getTicketStatus() }}</h5>
-                    </td>
-                </tr>
+        <a href="{{ action('TicketController@create') }}" class="btn btn-primary" role="button">New</a>
+        <a href="#" class="btn btn-primary" role="button">Merge</a>
+        <a href="#" class="btn btn-primary" role="button">Delete</a>
+        <a href="#" class="btn btn-primary" role="button">Close</a>
+        <a href="#" class="btn btn-primary" role="button">Assigned to</a>
 
-                @endforeach
+        <hr/>
 
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th>Select</th>
-                    <th>Title</th>
-                    <th>Create</th>
-                    <th>Owner</th>
-                    <th>Client</th>
-                    <th>Status</th>
-                </tr>
-            </tfoot>
-        </table>
-    </div><!-- /.box-body -->
-</div><!-- /.box -->
-@else
+        @if ( count($tickets) )
 
-<h3>There are no tickets.</h3>
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title">Ticket list</h3>
+            </div><!-- /.box-header -->
+            <div class="box-body">
+                <table id="tickets" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Select</th>
+                            <th>Title</th>
+                            <th>Create</th>
+                            <th>Owner</th>
+                            <th>Client</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ( $tickets as $ticket )
+                        <tr>
+                            <!-- Select -->
+                            <td>
+                                <input type="checkbox" name="option1" value="Selected"> 
+                            </td>
+                            <!-- Title -->
+                            <td>
+                                <div class="text-left">
+                                    <a href="{{ action('TicketController@show', $ticket->id ) }}"> {{ $ticket->title }}</a> 
+                                </div>
+                            </td>
+                            <!-- Create -->
+                            <td>
+                                {{ $ticket->created_at->diffForHumans() }}
+                            </td>
+                            <!-- Owner -->
+                            <td>
+                                @if ($ticket->assignedTo == null )
+                                <h5>unassigned</h5>
+                                @else
+                                <h5>{{ $ticket->assignedTo->name }}</h5>
+                                @endif
+                            </td>
+                            <!-- Client -->
+                            <td>
+                                @if ( $ticket->isFrom == null )
+                                <h5>unassigned</h5>
+                                @else
+                                <h5>{{ $ticket->isFrom->name }}</h5>
+                                @endif
+                            </td>
+                            <!-- Status -->
+                            <td>
+                                <h5>{{ $ticket->getTicketStatus() }}</h5>
+                            </td>
+                        </tr>
 
-@endif  
+                        @endforeach
+
+                    </tbody>
+                </table>
+            </div><!-- /.box-body -->
+        </div><!-- /.box -->
+        @else
+
+        <h3>There are no tickets.</h3>
+
+        @endif  
+
+    </div>
+</div>
 
 @stop   
 
--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -4,19 +4,62 @@
 
 @if ( count($ticket) )
 
-<h3>{{ $ticket->title }}</h3>
+<div class="box box-solid">
+    <div class="box-header with-border">
 
-<a href="/tickets/{{ $ticket->id }}/edit"><h5>Edit ticket</h5></a>
-<a href="/tickets"><h5>Back to ticket list</h5></a>
-<hr/>
+        <h3 class="box-title">{{ $ticket->title }}</h3>
+    </div>
+    <div class="box-body">
 
-<h5>Created at : {{$ticket->created_at->diffForHumans() }}</h5>
-<h5>Ticket id: {{ $ticket->id }}</h5>
-<h5>Status : {{ $ticket->getTicketStatus() }}</h5>
 
-<p>{{ $ticket->description }}</p>
+        <a href="/tickets/{{ $ticket->id }}/edit"><h5>Edit ticket</h5></a>
+        <a href="/tickets"><h5>Back to ticket list</h5></a>
+        <hr/>
 
-<hr/>
+        <h5>Ticket id: {{ $ticket->id }}</h5>
+        <h5>Status : {{ $ticket->getTicketStatus() }}</h5>
+    </div>
+</div>
+
+        <hr/>
+
+        <ul class="timeline">
+            <!-- timeline time label -->
+            <li class="time-label">
+                <span class="bg-red">
+                    {{$ticket->created_at}} ({{$ticket->created_at->diffForHumans() }})
+                </span>
+            </li>
+            <!-- /.timeline-label -->
+            <!-- timeline item -->
+            <li>
+                <i class="fa fa-envelope bg-blue"></i>
+                <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+                    <h3 class="timeline-header">Ticket created</h3>
+                    <div class="timeline-body">
+                        {{ $ticket->description }}
+                    </div>
+                    <div class="timeline-footer">
+                        <a class="btn btn-primary btn-xs">Read more</a>
+                        <a class="btn btn-danger btn-xs">Delete</a>
+                    </div>
+                </div>
+            </li>
+            <!-- END timeline item -->
+            <!-- timeline item -->
+            <li>
+                <i class="fa fa-user bg-aqua"></i>
+                <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 5 mins ago</span>
+                    <h3 class="timeline-header no-border"><a href="#"> Demo</a> now ownes this ticket</h3>
+                </div>
+            </li>
+            <!-- END timeline item -->
+            <li>
+                <i class="fa fa-clock-o bg-gray"></i>
+            </li>
+        </ul>
 
 @else
 
